2f5e30eb5372cdfebe605a9136ba297cdba0830f
FegrusB <fergus@beckerleg.co.uk>
2023-06-07 10:10:19 +0100

    07/06/2023 Initial Setup

---
+++ Database/init/ApprovalServerDatabase.sql
@@ -0,0 +1,65 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE documents (
+    document_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    customer VARCHAR(30) NOT NULL,
+    project VARCHAR(30) NOT NULL,
+    document_link VARCHAR(255)
+);
+
+CREATE TABLE users (
+    user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    pass_hash VARCHAR(255),
+    token VARCHAR(255),
+    UNIQUE (token)
+);
+
+CREATE TABLE chain (
+    chain_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    document_id INT NOT NULL,
+    user_id INT NOT NULL,
+    position INT NOT NULL,
+    time_stamp DATETIME,
+    FOREIGN KEY (document_id) REFERENCES documents (document_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+INSERT INTO documents (customer, project, document_link) VALUES ('HMRC', 'Springboot-app', 'sharepoint.com/test');
+INSERT INTO documents (customer, project, document_link) VALUES ('DWP', 'AWS Migration', 'sharepoint.com/test');
+INSERT INTO documents (customer, project, document_link) VALUES ('NASA', 'Machine learning project', 'sharepoint.com/test');
+INSERT INTO documents (customer, project, document_link) VALUES ('V1 Newcastle', 'New office Strawberry place', 'sharepoint.com/test');
+
+INSERT INTO users (user_name, email) VALUES ('Andrew H','Andrew.H@version1.com');
+INSERT INTO users (user_name, email) VALUES ('Arsalan A','Arsalan.A@version1.com');
+INSERT INTO users (user_name, email) VALUES ('Connor O','Connor.O@version1.com');
+INSERT INTO users (user_name, email) VALUES ('Dan P','Dan.P@version1.com');
+INSERT INTO users (user_name, email) VALUES ('Eunjung S','Eunjung.S@version1.com');
+INSERT INTO users (user_name, email) VALUES ('Fergus B','Fergus.B@version1.com');
+INSERT INTO users (user_name, email) VALUES ('Olfa K','Olfa.K@version1.com');
+INSERT INTO users (user_name, email) VALUES ('Sumaiya A','Sumaiya.A@version1.com');
+INSERT INTO users (user_name, email) VALUES ('Vasi C','Vasi.C@version1.com');
+
+INSERT INTO chain (document_id, user_id, position) VALUES(1,1,1);
+INSERT INTO chain (document_id, user_id, position) VALUES(1,4,2);
+INSERT INTO chain (document_id, user_id, position) VALUES(1,9,3);
+INSERT INTO chain (document_id, user_id, position) VALUES(1,3,4);
+
+INSERT INTO chain (document_id, user_id, position) VALUES(2,6,1);
+INSERT INTO chain (document_id, user_id, position) VALUES(2,5,2);
+INSERT INTO chain (document_id, user_id, position) VALUES(2,8,3);
+
+INSERT INTO chain (document_id, user_id, position) VALUES(3,2,1);
+
+INSERT INTO chain (document_id, user_id, position) VALUES(4,1,1);
+INSERT INTO chain (document_id, user_id, position) VALUES(4,2,2);
+INSERT INTO chain (document_id, user_id, position) VALUES(4,3,3);
+INSERT INTO chain (document_id, user_id, position) VALUES(4,4,4);
+INSERT INTO chain (document_id, user_id, position) VALUES(4,5,5);
+INSERT INTO chain (document_id, user_id, position) VALUES(4,6,6);
+INSERT INTO chain (document_id, user_id, position) VALUES(4,7,7);
+INSERT INTO chain (document_id, user_id, position) VALUES(4,8,8);
+INSERT INTO chain (document_id, user_id, position) VALUES(4,9,9);
---
+++ src/test/java/com/teamone/approvalserver/ApprovalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamone.approvalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
