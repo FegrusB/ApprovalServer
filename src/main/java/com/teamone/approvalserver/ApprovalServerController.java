a5e5e70848a37879dd77ee1f58a8aa0d6539fcc1
Sumaiya A <107862616+May-95@users.noreply.github.com>
2023-06-12 11:43:41 +0100

    getDocumentById method added

--- src/main/java/com/teamone/approvalserver/ApprovalServerController.java
+++ src/main/java/com/teamone/approvalserver/ApprovalServerController.java
@@ -1,9 +1,15 @@
 package com.teamone.approvalserver;
 
+import com.teamone.approvalserver.Models.DocumentModel;
+import com.teamone.approvalserver.Services.DocumentService;
 import com.teamone.approvalserver.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/V1Approved")
@@ -11,8 +17,16 @@ public class ApprovalServerController {
 
     UserService userService;
 
+    private final DocumentService documentService;
+
     @Autowired
-    public ApprovalServerController(UserService userService) {
+    public ApprovalServerController(UserService userService, DocumentService documentService) {
         this.userService = userService;
+        this.documentService = documentService;
     }
+
+    @GetMapping("/{documentId}/info")
+    public Optional<DocumentModel> getDocumentInfo(@PathVariable("documentId") Integer documentId) {
+            return documentService.getDocumentInfo(documentId);
+    };
 }
--- src/main/java/com/teamone/approvalserver/Services/DocumentService.java
+++ src/main/java/com/teamone/approvalserver/Services/DocumentService.java
@@ -1,10 +1,26 @@
 package com.teamone.approvalserver.Services;
 
+import com.teamone.approvalserver.Models.DocumentModel;
+import com.teamone.approvalserver.Repositories.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * This Service constructs Document and chain objects to be Inserted into the database.
  */
 @Service
 public class DocumentService {
+
+    private final DocumentRepository documentRepository;
+
+    @Autowired
+    public DocumentService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+
+   public Optional<DocumentModel> getDocumentInfo(Integer documentId) {
+       return documentRepository.findById(documentId);
+   }
 }
